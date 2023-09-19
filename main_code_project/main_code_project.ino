789634ef7c605ebeffcff40edbecbc9d60a03ac5
joelotfy <ejrgfjg@gmail.com>
2023-09-19 19:15:16 +0300

    Final Clean on main project's code

--- main_code_project/main_code_project.ino
+++ main_code_project/main_code_project.ino
@@ -18,7 +18,7 @@
 
 #define motor3_1 6
 #define motor3_2 7
-/*
+
 #define motor4_1 10
 #define motor4_2 9
 
@@ -27,7 +27,7 @@
 
 #define motor6_1 10
 #define motor6_2 9
-*/
+
 #define motor7_1 2  //  car right 
 #define motor7_2 3  // car right
 
@@ -47,8 +47,6 @@ byte vaibrate = 0;
 
 //functions
 //arm
-
-
 void hold(){
   digitalWrite(motor1_1, HIGH);
   digitalWrite(motor1_2, LOW);
@@ -59,7 +57,7 @@ void let(){
   digitalWrite(motor1_2, HIGH);
 }
 /////////////////////////////////////////////
-/*
+
 void curve_up(){
   digitalWrite(motor2_1, HIGH);
   digitalWrite(motor2_2, LOW);
@@ -69,7 +67,6 @@ void curve_down(){
   digitalWrite(motor2_1, LOW);
   digitalWrite(motor2_2, HIGH);
 } 
-*/
 /////////////////////////////////////////////
 
 void up(){
@@ -82,7 +79,7 @@ void down(){
   digitalWrite(motor3_2, HIGH);
 }
 /////////////////////////////////////////////
-/* 
+ 
 void forward(){
   digitalWrite(motor4_1, HIGH);
   digitalWrite(motor4_2, LOW);
@@ -115,7 +112,7 @@ void spray_off(){
   digitalWrite(motor6_2, HIGH);
 }
 //////////////////////////////////////////
-*/
+
 //car
 void backward_car(){
   digitalWrite(motor7_1, LOW);  // right motor
@@ -151,17 +148,15 @@ void Left_car(){
   digitalWrite(motor8_1, HIGH);  // left motor
   digitalWrite(motor8_2, LOW);  // left motor
 }
-/////////////////////////////////////////////
+////////////////////////////////////////////////
+
 //led
-void led_on()
-{
-digitalWrite(led, HIGH);
+void led_on(){
+  digitalWrite(led, HIGH);
 }
 
-void led_off()
-{
-digitalWrite(led, LOW);
-
+void led_off(){
+  digitalWrite(led, LOW);
 }
 void setup(){
   Serial.begin(9600);
@@ -173,29 +168,27 @@ void setup(){
     Serial.println("error");
   }
 
-pinMode(motor1_1, OUTPUT);        
-pinMode(motor1_2, OUTPUT);  
+  pinMode(motor1_1, OUTPUT);        
+  pinMode(motor1_2, OUTPUT);  
 
 
-//pinMode(motor2_1, OUTPUT);        
-//pinMode(motor2_2, OUTPUT);
+  pinMode(motor2_1, OUTPUT);        
+  pinMode(motor2_2, OUTPUT);
 
-pinMode(motor3_1, OUTPUT);        
-pinMode(motor3_2, OUTPUT);
-/*
-pinMode(motor4_1, OUTPUT);        
-pinMode(motor4_2, OUTPUT);
+  pinMode(motor3_1, OUTPUT);        
+  pinMode(motor3_2, OUTPUT);
 
-pinMode(motor5_1, OUTPUT);
-pinMode(motor5_2, OUTPUT);
+  pinMode(motor4_1, OUTPUT);        
+  pinMode(motor4_2, OUTPUT);
+
+  pinMode(motor5_1, OUTPUT);
+  pinMode(motor5_2, OUTPUT);
 
 
-pinMode(motor6_1, OUTPUT);
-pinMode(motor6_2, OUTPUT);
-*/
+  pinMode(motor6_1, OUTPUT);
+  pinMode(motor6_2, OUTPUT);
 
-//car
-
+  //car
   pinMode(motor7_1, OUTPUT);
   pinMode(motor7_2, OUTPUT);
   pinMode(motor8_1, OUTPUT);        
@@ -204,30 +197,16 @@ pinMode(motor6_2, OUTPUT);
   //pinMode(pin2_speed, OUTPUT);
 
 
-pinMode(led, OUTPUT);
-//configration buttons
-
-/*ps2x.Button(PSB_L1);
-ps2x.Button(PSB_L2);
-ps2x.Button(PSB_L3);
-ps2x.Button(PSB_R1);
-ps2x.Button(PSB_R2);
-ps2x.Button(PSB_R3);
-ps2x.Button(PSB_PAD_UP);
-ps2x.Button(PSB_PAD_DOWN);
-ps2x.Button(PSB_PAD_RIGHT);
-ps2x.Button(PSB_PAD_LEFT);  
-ps2x.Button(PSB_START);
-ps2x.Button(PSB_SELECT);
-*/
-
+  pinMode(led, OUTPUT);
 }
-void loop(){
+
+
+void loop() {
   ps2x.read_gamepad(false,vaibrate); //vibrate all time
   
   // if cases 
 
-   if(ps2x.Button(PSB_L1)!=0){
+  if(ps2x.Button(PSB_L1)!=0){
     Serial.println("L1 pressed");
     let();
   }
@@ -240,8 +219,8 @@ void loop(){
     digitalWrite(motor1_2, LOW);
   }
 /////////////////////////////////////////////
-/*
-if(ps2x.Button(PSB_PINK)!=0){
+
+  if(ps2x.Button(PSB_PINK)!=0){
     Serial.println("square pressed");
     curve_down();
   }
@@ -253,8 +232,8 @@ if(ps2x.Button(PSB_PINK)!=0){
     digitalWrite(motor2_1, LOW);
     digitalWrite(motor2_2, LOW);
   }
-  */
-/////////////////////////////////////////////
+  /////////////////////////////////////////////
+
   if(ps2x.Button(PSB_GREEN)!=0){
     Serial.println("throw pressed");
     up();
@@ -267,9 +246,9 @@ if(ps2x.Button(PSB_PINK)!=0){
     digitalWrite(motor3_1, LOW);
     digitalWrite(motor3_2, LOW);
   }
-/////////////////////////////////////////////
-/*
- if(ps2x.Button(PSB_PAD_UP)!=0){
+  ////////////////////////////////////////////////////////
+
+  if(ps2x.Button(PSB_PAD_UP)!=0){
     Serial.println("UP pressed");
     forward();
   }
@@ -281,9 +260,9 @@ if(ps2x.Button(PSB_PINK)!=0){
     digitalWrite(motor4_1, LOW);
     digitalWrite(motor4_2, LOW);
   }
-/////////////////////////////////////////////
+  /////////////////////////////////////////////
 
- if(ps2x.Button(PSB_PAD_RIGHT)!=0){
+  if(ps2x.Button(PSB_PAD_RIGHT)!=0){
     Serial.println("right pressed");
     right();
   }
@@ -295,7 +274,7 @@ if(ps2x.Button(PSB_PINK)!=0){
     digitalWrite(motor5_1, LOW);
     digitalWrite(motor5_2, LOW);
   }
-/////////////////////////////////////////////
+  //////////////////////////////////////////////////////////
 
   if(ps2x.Button(PSB_R1)!=0){
     Serial.println("R1 pressed");
@@ -309,11 +288,9 @@ if(ps2x.Button(PSB_PINK)!=0){
     digitalWrite(motor6_1, LOW);
     digitalWrite(motor6_2, LOW);
   }
-/////////////////////////////////////////////
-*/
+  /////////////////////////////////////////////
 
-if(ps2x.Analog(PSS_LX)||ps2x.Analog(PSS_LY))
-  {
+  if(ps2x.Analog(PSS_LX)||ps2x.Analog(PSS_LY)){
     Serial.print(ps2x.Analog(PSS_LX),DEC);
     Serial.print(",");
     Serial.println(ps2x.Analog(PSS_LY),DEC);
@@ -327,16 +304,16 @@ if(ps2x.Analog(PSS_LX)||ps2x.Analog(PSS_LY))
     else if(ps2x.Analog(PSS_LY)==127||ps2x.Analog(PSS_LX)==128){
       stop_car();
     }
-   if(ps2x.Analog(PSS_LX)<128){
+    if(ps2x.Analog(PSS_LX)<128){
       Left_car();
     }
     else if(ps2x.Analog(PSS_LX)>128){
       Right_car();    
     }
   }
-/////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////
 
- if(ps2x.Button(PSB_START)){
+  if(ps2x.Button(PSB_START)){
     Serial.println("START pressed");
     led_on();
   }
@@ -344,21 +321,14 @@ if(ps2x.Analog(PSS_LX)||ps2x.Analog(PSS_LY))
     Serial.println("SELECT pressed");
     led_off();
   }
-/////////////////////////////////////////////
+  /////////////////////////////////////////////
 
-
-
-/*
   if(ps2x.Button(PSB_L3)){
     Serial.println("L3 pressed");
   }
-
-
   if(ps2x.Button(PSB_R3)){
     Serial.println("R3 pressed");  
   }
-*/
  
 delay(30);
-
 }
