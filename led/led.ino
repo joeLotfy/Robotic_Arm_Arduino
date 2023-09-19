c269911a6e7c490916162b70c278fb749381e2fa
joelotfy <ejrgfjg@gmail.com>
2023-09-19 19:14:05 +0300

    Updated led prototype code

--- led/led.ino
+++ led/led.ino
@@ -2,23 +2,23 @@
 int i=0;
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-if(ps2.start){
+  if(ps2.start){
     if (i%2=0){ 
-    digitalWrite(led, HIGH);
-    delay(500);
-    digitalWrite(led, HIGH);
-    i++;
+      digitalWrite(led, HIGH);
+      delay(500);
+      digitalWrite(led, HIGH);
+      i++;
     }
     else {
-    digitalWrite(led, LOW);
-    delay(500);
-    digitalWrite(led, LOW);
-    i++;
+      digitalWrite(led, LOW);
+      delay(500);
+      digitalWrite(led, LOW);
+      i++;
     }
   }
 }
