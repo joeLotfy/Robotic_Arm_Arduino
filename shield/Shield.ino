024d477b569bb68c56ea76de942c01b0eb72796c
Yusuf A. HassanLotfy Ali <62353944+joeLotfy@users.noreply.github.com>
2023-09-20 13:57:17 +0300

    Update Shield.ino (Not Tested)
    deleted some useless comments, added new declarative comments

--- shield/Shield.ino
+++ shield/Shield.ino
@@ -1,7 +1,9 @@
 //لا حول ولا قوة ولا بالله
+//L293d motor driver arduino shield
 #include <PS2X_lib.h>
 #include <AFMotor.h>
 
+//define motors
 //AF_DCMotor Motor1(1,MOTOR12_64KHZ);
 AF_DCMotor Motor1(1);
 AF_DCMotor Motor2(2);
@@ -11,8 +13,8 @@ AF_DCMotor Motor4(4);
 
 
 void setup() {
-  // put your setup code here, to run once:
 	Serial.begin(9600);
+	//set speed 0~255
 	Motor1.setSpeed(180);
 	Motor2.setSpeed(180);
 	Motor3.setSpeed(180);
@@ -20,7 +22,7 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	//just keep forward for the sake of the test
 	Motor1.run(FORWARD);
 	Motor2.run(FORWARD);
 	Motor3.run(FORWARD);
